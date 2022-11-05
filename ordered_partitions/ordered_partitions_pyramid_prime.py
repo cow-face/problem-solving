f6634f8328389136bf4902c9ebaeb9a0d51f11a4
cow-face <caden.unnasch@gmail.com>
2022-11-05 09:33:41 -0500

    added comments

--- ordered_partitions/ordered_partitions_pyramid_prime.py
+++ ordered_partitions/ordered_partitions_pyramid_prime.py
@@ -1,4 +1,12 @@
-#Working on a solution for Mathematics Magazine Problem #2154
+# Working on a solution for Mathematics Magazine Problem #2154
+#
+# Given user input n, prints out the number of ordered partitions of n with only odd parts with
+# m terms from 1 to n.
+# So since 5 can be written as 5, 1+1+3, 1+3+1, 3+1+1, or 1+1+1+1+ 1, the 5 row will print 
+# out 1 0 3 0 5. This is because five has 1 single integer partition, 0 two integer partitions, 3 three
+# integer partitions, 4 four integer partitions, and 1 five integer partition.
+#
+# The primes also print marked with a ]
 
 def f(n):
     answer = set()
